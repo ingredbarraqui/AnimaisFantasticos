6c2baee097365469baa2d101d67bd505538ef612
ingredbarraqui <ingredbarraqui@users.noreply.github.com>
2021-05-05 13:47:18 -0300

    initAcordion

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li")
-const tabContent = document.querySelectorAll(".js-tabcontent section")
+function initTabNav() {
+    const tabMenu = document.querySelectorAll(".js-tabmenu li")
+    const tabContent = document.querySelectorAll(".js-tabcontent section")
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo")
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add("ativo")
 
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove("ativo")
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove("ativo")
+            });
+            tabContent[index].classList.add("ativo");
+        }
+
+        tabMenu.forEach((item, index) => {
+            item.addEventListener("click", function () {
+                activeTab(index)
+            })
         });
-        tabContent[index].classList.add("ativo");
     }
-
-    tabMenu.forEach((item, index) => {
-        item.addEventListener("click", function () {
-            activeTab(index)
-        })
-    });
 }
+initTabNav()
+
+function initAcordion() {
+    const acordionList = document.querySelectorAll(".js-acordion dt")
+    if (acordionList.length) {
+        acordionList[0].classList.add("ativo")
+        acordionList[0].nextElementSibling.classList.add("ativo")
+
+        function activeAcordion(event) {
+            event.currentTarget.classList.toggle("ativo")
+            event.currentTarget.nextElementSibling.classList.toggle("ativo")
+        }
+        acordionList.forEach((item) => {
+            item.addEventListener("click", activeAcordion)
+        })
+
+    }
+}
+initAcordion()
 
 
